0167478609f410ba7d159cc848fc55a5f2e33d49
sobhanhsa <sobhanb7b7@protonmail.com>
2023-11-01 10:51:34 +0330

    first

---
+++ Example.py
@@ -0,0 +1,111 @@
+# @ 2020, Copyright Amirmohammad Zarif
+# Compatible with firasimulator version 1.0.1 or higher
+import FiraAuto
+import time
+import cv2
+import numpy as np
+from utils import make_poly
+from utils import draw_lines
+from utils import calc_avg_line
+#Calling the class
+car = FiraAuto.car()
+
+#connecting to the server (Simulator)
+car.connect("127.0.0.1", 25001)
+
+#Counter variable
+counter = 0
+
+debug_mode = False
+#sleep for 2 second to make sure that client connected to the simulator 
+time.sleep(2)
+global steering
+steering = 0
+try:
+    while(True):
+
+        print(steering)
+
+        #Counting the loops
+        
+        counter = counter + 1
+
+        #Set the power of the engine the car to 20, Negative number for reverse move, Range [-100,100]
+        car.setSpeed(60)
+
+        #Set the Steering of the car -10 degree from center
+        car.setSteering(steering)
+
+        #Get the data. Need to call it every time getting image and sensor data
+        car.getData()
+
+        #Start getting image and sensor data after 4 loops. for unclear some reason it's really important 
+        if(counter > 4):
+            #returns a list with three items which the 1st one is Left sensor data, the 2nd one is the Middle Sensor data, and the 3rd is the Right one.
+            sensors = car.getSensors() 
+            #EX) sensors[0] returns an int for left sensor data in cm
+
+            #returns an opencv image type array. if you use PIL you need to invert the color channels.
+            _image = car.getImage()
+
+            if _image is None:
+                print('None image received!!    ')
+                continue
+
+            carSpeed = car.getSpeed()
+            
+            image = _image.copy()
+
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            blur = cv2.GaussianBlur(gray, (3,15), 0)
+
+            edge = cv2.Canny(blur, 200, 300)
+
+            blank = np.zeros_like(edge)	
+
+            height, width = edge.shape	
+
+            poly = make_poly(width,height)
+
+            mask_shape = cv2.fillPoly(blank, pts=[poly], color=255)
+     
+            mask_img = cv2.bitwise_and(edge,mask_shape)
+
+            lines = cv2.HoughLinesP(mask_img, rho=3, theta=np.pi/90, threshold=20, lines=np.array([]), minLineLength=10, maxLineGap=5)
+
+
+            if lines is None:
+                print('no line detected!')
+                continue
+
+            if (len(lines) == 1):
+                lines = [lines]
+
+            avg_lines , error = calc_avg_line(blank.copy(),lines) 
+
+            steering = -error
+
+            lines_img = draw_lines(image.copy(),lines,(0,255,0))
+
+            #Don't print data for better performance
+            if(debug_mode):
+                print("Speed : ",carSpeed) 
+                #currently the angle between the sensors is 30 degree TODO : be able to change that from conf.py
+                print("Left : " + str(sensors[0]) + "   |   " + "Left : " + str(sensors[1])  +"   |   " + "Left : " + str(sensors[2]))
+
+            #showing the opencv type image
+            cv2.imshow('frames', lines_img)
+            #break the loop when q pressed
+            if cv2.waitKey(10) == ord('q'):
+                break
+            time.sleep(0.001)
+        #A brief sleep to make sure everything 
+        
+finally:
+    car.stop()
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,75 @@
+import cv2	
+import numpy as np
+
+def make_poly(width,height,i = 30, j = 160, k = 0 ):
+	
+    i = 15
+
+    j = 0
+
+    k = 0
+
+    return np.array([
+    [
+        (0, height - k),
+        (int(width / 2) - i , int(height / 2) + j),
+        (int(width / 2) + i , int(height / 2) + j),
+        (width, height -k)
+    ]
+    ])
+
+def translate(inp_min, inp_max, out_min, out_max,inp_error):
+	return (float(inp_error - inp_min) * float(out_max-out_min) / float(inp_max - inp_min) + float(out_min))
+
+def draw_lines(image, lines, color):
+	#line_image = image.copy()
+	for line in lines:
+	# print(lines[0])
+		x1, y1, x2, y2 = line.reshape(4) 
+		# print(type(x1))
+		cv2.line(image, (x1, y1) ,(x2, y2), color, 10)			
+	return image
+
+def make_coordinates(image, line):
+	if(isinstance(line, np.ndarray)):
+		slope, intercept = line
+		y1 = image.shape[0]
+		y2 = int(y1 * (3/5))
+		x1 = int((y1 - intercept) / slope)
+		x2 = int((y2 - intercept) / slope)
+		return np.array([x1, y1, x2, y2])
+	else:
+		return np.array([0,0,0,0])
+
+def calc_avg_line(image, lines):
+	left_lines = []
+	right_lines = []
+	
+	for line in lines:
+		x1, y1, x2, y2 = line.reshape(4) 
+		parameters = np.polyfit((x1,x2), (y1,y2), 1)
+		slope = parameters[0]
+		intercept = parameters[1]
+		if(slope <= 0):
+			if(slope != 0):
+				left_lines.append((slope, intercept))
+				# left_lines.append((slope, intercept))
+		else:
+			right_lines.append((slope, intercept))
+
+	left_line_avg = np.average(left_lines, axis=0) 
+	right_line_avg = np.average(right_lines, axis=0) 
+
+
+	left_line = make_coordinates(image, left_line_avg)
+	right_line = make_coordinates(image, right_line_avg)
+	
+	left_line_avg_fix = left_line_avg if (isinstance(left_line_avg, np.ndarray)) else np.array([0, 0]) 	
+
+	
+	right_line_avg_fix = right_line_avg if (isinstance(right_line_avg, np.ndarray)) else np.array([0, 0]) 	
+	
+	error = (left_line_avg_fix[0] + right_line_avg_fix[0]) / 2
+		
+ 
+	return np.array([left_line, right_line]), error
