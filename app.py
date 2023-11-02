10f8080756d3b6001b32322b7c9b9499bd6b2bc0
Amin <mohanmad1386tabrizi4@email.com>
2023-11-02 17:55:40 +0330

    feat: update app.py & add some files

--- app.py
+++ app.py
@@ -1,68 +1,75 @@
-import FiraAuto
+import avisengine
+import config
 import time
 import cv2
 import numpy as np
-from utils import make_poly
-from utils import draw_lines
-from utils import calc_avg_line
-#Calling the class
-car = FiraAuto.car()
+from required import *
 
-#connecting to the server (Simulator)
-car.connect("127.0.0.1", 25001)
+# Creating an instance of the Car class
+car = avisengine.Car()
 
-#Counter variable
+# Connecting to the server (Simulator)
+car.connect(config.SIMULATOR_IP, config.SIMULATOR_PORT)
+
+# Counter variable
 counter = 0
+steering = 0
 
 debug_mode = False
-#sleep for 2 second to make sure that client connected to the simulator 
-time.sleep(2)
-global steering
-steering = 0
+
+# Sleep for 3 seconds to make sure that client connected to the simulator 
+time.sleep(3)
+
 try:
     while(True):
 
-        print(steering)
-
-        #Counting the loops
-        
+        # Counting the loops
         counter = counter + 1
 
-        #Set the power of the engine the car to 20, Negative number for reverse move, Range [-100,100]
-        car.setSpeed(60)
+        # Set the power of the engine the car to 20, Negative number for reverse move, Range [-100,100]
+        car.setSpeed(50)
 
-        #Set the Steering of the car -10 degree from center
+        # Set the Steering of the car -10 degree from center, results the car to steer to the left
         car.setSteering(steering)
+        
+        # Set the angle between sensor rays to 45 degrees, Use this only if you want to set it from python client
+        # Notice: Once it is set from the client, it cannot be changed using the GUI
+        car.setSensorAngle(45) 
 
-        #Get the data. Need to call it every time getting image and sensor data
+        # Get the data. Need to call it every time getting image and sensor data
         car.getData()
 
-        #Start getting image and sensor data after 4 loops. for unclear some reason it's really important 
+        # Start getting image and sensor data after 4 loops
         if(counter > 4):
-            #returns a list with three items which the 1st one is Left sensor data, the 2nd one is the Middle Sensor data, and the 3rd is the Right one.
+
+            # Returns a list with three items which the 1st one is Left sensor data\
+            # the 2nd one is the Middle Sensor data, and the 3rd is the Right one.
             sensors = car.getSensors() 
-            #EX) sensors[0] returns an int for left sensor data in cm
 
-            #returns an opencv image type array. if you use PIL you need to invert the color channels.
+            # Returns an opencv image type array. if you use PIL you need to invert the color channels.
             _image = car.getImage()
-
+            
             if _image is None:
-                print('None image received!!    ')
+                print('image not received!')
                 continue
 
-            carSpeed = car.getSpeed()
-            
             image = _image.copy()
 
+            # Returns an integer which is the real time car speed in KMH
+            carSpeed = car.getSpeed()
+
+            # get a gray image from the original image
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+            # make our grayimage to blur
             blur = cv2.GaussianBlur(gray, (3,15), 0)
 
+            # get our image's edges
             edge = cv2.Canny(blur, 200, 300)
 
             blank = np.zeros_like(edge)	
 
-            height, width = edge.shape	
+            width, height = edge.shape	
 
             poly = make_poly(width,height)
 
@@ -72,38 +79,40 @@ try:
 
             lines = cv2.HoughLinesP(mask_img, rho=3, theta=np.pi/90, threshold=20, lines=np.array([]), minLineLength=10, maxLineGap=5)
 
+            color = (0,255,0)
 
+            # Show the results if we had lines
             if lines is None:
                 print('no line detected!')
                 continue
 
-            if (len(lines) == 1):
-                lines = [lines]
+            else:
+                car.setSpeed(5)
 
-            avg_lines , error = calc_avg_line(blank.copy(),lines) 
+                if (len(lines) == 1):
+                    lines = [lines]
 
-            steering = -error
+                avg_lines , error = calc_avg_line(blank.copy(),lines) 
+        
 
-            lines_img = draw_lines(image.copy(),lines,(0,255,0))
+            steering = translate(-1.5,1.5,-4.5,4.5,float(error))
 
-            #Don't print data for better performance
+            lines_img = draw_lines(image,lines,(0,255,0))
+
+
             if(debug_mode):
-                print("Speed : ",carSpeed) 
-                #currently the angle between the sensors is 30 degree TODO : be able to change that from conf.py
-                print("Left : " + str(sensors[0]) + "   |   " + "Left : " + str(sensors[1])  +"   |   " + "Left : " + str(sensors[2]))
+                print(f"Speed : {carSpeed}") 
+                print(f'Left : {str(sensors[0])} | Middle : {str(sensors[1])} | Right : {str(sensors[2])}')
+            
+            # Showing the opencv type image
+            cv2.imshow('frames', image)
+            cv2.imshow('canny', edge)
 
-            #showing the opencv type image
-            cv2.imshow('frames', lines_img)
-            #break the loop when q pressed
+
             if cv2.waitKey(10) == ord('q'):
                 break
+
             time.sleep(0.001)
-        #A brief sleep to make sure everything 
-        
+
 finally:
     car.stop()
-
-
-
-
-
--- utils.py
+++ utils.py
@@ -1,69 +1,98 @@
-import cv2	
+'''
+@ 2022, Copyright AVIS Engine
+'''
+# Utils for AVISEngine Class
+import base64
+import cv2
 import numpy as np
-
-def make_poly(width,height,i = 15, j = 0, k = 0 ):
-
-    return np.array([
-    [
-        (0, height - k),
-        (int(width / 2) - i , int(height / 2) + j),
-        (int(width / 2) + i , int(height / 2) + j),
-        (width, height -k)
-    ]
-    ])
-
-def translate(inp_min, inp_max, out_min, out_max,inp_error):
-	return (float(inp_error - inp_min) * float(out_max-out_min) / float(inp_max - inp_min) + float(out_min))
-
-def draw_lines(image, lines, color):
-	#line_image = image.copy()
-	for line in lines:
-	# print(lines[0])
-		x1, y1, x2, y2 = line.reshape(4) 
-		# print(type(x1))
-		cv2.line(image, (x1, y1) ,(x2, y2), color, 10)			
-	return image
-
-def make_coordinates(image, line):
-	if(isinstance(line, np.ndarray)):
-		slope, intercept = line
-		y1 = image.shape[0]
-		y2 = int(y1 * (3/5))
-		x1 = int((y1 - intercept) / slope)
-		x2 = int((y2 - intercept) / slope)
-		return np.array([x1, y1, x2, y2])
-	else:
-		return np.array([0,0,0,0])
-
-def calc_avg_line(image, lines):
-	left_lines = []
-	right_lines = []
-	
-	for line in lines:
-		x1, y1, x2, y2 = line.reshape(4) 
-		parameters = np.polyfit((x1,x2), (y1,y2), 1)
-		slope = parameters[0]
-		intercept = parameters[1]
-		if(slope <= 0):
-			if(slope != 0):
-				left_lines.append((slope, intercept))
-				# left_lines.append((slope, intercept))
-		else:
-			right_lines.append((slope, intercept))
-
-	left_line_avg = np.average(left_lines, axis=0) 
-	right_line_avg = np.average(right_lines, axis=0) 
+from PIL import Image
+import io
 
 
-	left_line = make_coordinates(image, left_line_avg)
-	right_line = make_coordinates(image, right_line_avg)
-	
-	left_line_avg_fix = left_line_avg if (isinstance(left_line_avg, np.ndarray)) else np.array([0, 0]) 	
+__author__ = "Amirmohammad Zarif"
+__email__ = "amirmohammadzarif@avisengine.com"
 
-	
-	right_line_avg_fix = right_line_avg if (isinstance(right_line_avg, np.ndarray)) else np.array([0, 0]) 	
-	
-	error = (left_line_avg_fix[0] + right_line_avg_fix[0]) / 2
-		
- 
-	return np.array([left_line, right_line]), error
+def stringToImage(base64_string):
+    '''
+    Converts Base64 String to Image
+
+    Parameters
+    ----------
+    base64_string : str
+        base64 image data to be converted
+    '''
+    imgdata = base64.b64decode(base64_string)
+    return Image.open(io.BytesIO(imgdata))
+
+def BGRtoRGB(image):
+    '''
+    Converts PIL Image to an RGB image(technically a numpy array) that's compatible with opencv
+    '''
+    return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+
+def KMPSearch(pat, txt):
+    '''
+    Knuth-Morris-Pratt(KMP) Algorithm : Efficient Search Algorithm to search for a pattern in a string.
+    
+    Parameters
+    ----------
+        pat: str
+            Pattern to be searched for
+
+        txt: str
+            Text to search in
+    '''
+    M = len(pat)
+    N = len(txt)
+  
+    # create lps[] that will hold the longest prefix suffix 
+    # values for pattern
+    lps = [0] * M
+    j = 0 # index for pat[]
+
+    # Preprocess the pattern (calculate lps[] array)
+    computeLPS(pat, M, lps)
+  
+    i = 0 # Index for txt[]
+    while i < N:
+        if pat[j] == txt[i]:
+            i += 1
+            j += 1
+  
+        if j == M:
+            return (i-j)
+            
+  
+        # Mismatch after j matches
+        elif i < N and pat[j] != txt[i]:
+            # Do not match lps[0..lps[j-1]] characters.
+            if j != 0:
+                j = lps[j-1]
+            else:
+                i += 1
+    return -1
+  
+def computeLPS(pat, M, lps):
+    '''
+    Computing the LPS
+    '''
+    len = 0 # length of the previous longest prefix suffix
+  
+    lps[0] # lps[0] is always 0
+    i = 1
+  
+    # The loop calculates lps[i] for i = 1 to M-1
+    while i < M:
+        if pat[i]== pat[len]:
+            len += 1
+            lps[i] = len
+            i += 1
+        else:
+            # To search step.
+            if len != 0:
+                len = lps[len-1]
+            else:
+                lps[i] = 0
+                i += 1
+
+
